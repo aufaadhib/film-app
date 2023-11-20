1238087d1db31dff499cbcb09061db869bd42c45
Aufaadhib <faufaadhib@gmail.com>
2023-11-20 15:19:54 +0700

    Pertama

---
+++ src/api/apiConfig.js
@@ -0,0 +1,8 @@
+const apiConfig = {
+  baseUrl: "https://api.themovieedb.org/3/",
+  apiKey: "d74c3b5221cff669351f54eef2478d39",
+  OriginalImage: (imgPath) => "https://image.tmdb.org/t/p/w500",
+  w500Image: (imgPath) => "https://image.tmdb.org/t/p/w500",
+};
+
+export default apiConfig
